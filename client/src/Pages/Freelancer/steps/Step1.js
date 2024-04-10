05cb82a0432d16d087a9f14a7a5fa5c13a9419e2
Sinda07 <guesmisinda123@gmail.com>
2024-04-10 03:16:05 +0100

    design

--- client/src/Pages/Freelancer/steps/Step1.js
+++ client/src/Pages/Freelancer/steps/Step1.js
@@ -1,4 +1,3 @@
-// Step1.js
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudUploadAlt } from "@fortawesome/free-solid-svg-icons";
@@ -19,51 +18,53 @@ function Step1({
   };
 
   return (
-    <div className={`step ${isActive ? "active-step" : ""}`}>
-      <h2 className="step-heading">Step 1: Your Info</h2>
-      <div className="image-upload">
-        <label htmlFor="profile-pic" className="image-upload-label">
-          {formData.pic ? (
-            <img src={formData.pic} alt="Profile" className="profile-pic" />
-          ) : (
-            <React.Fragment>
-              <FontAwesomeIcon
-                icon={faCloudUploadAlt}
-                className="upload-icon"
-              />
-              <span className="" style={{ color: "white" }}>
-                Upload Profile Picture
-              </span>
-            </React.Fragment>
-          )}
-        </label>
+    <div className="step-container">
+      <div className={`step ${isActive ? "active-step" : ""}`}>
+        <h2 className="step-heading">Step 1: Your Info</h2>
+        <div className="image-upload">
+          <label htmlFor="profile-pic" className="image-upload-label">
+            {formData.pic ? (
+              <img src={formData.pic} alt="Profile" className="profile-pic" />
+            ) : (
+              <React.Fragment>
+                <FontAwesomeIcon
+                  icon={faCloudUploadAlt}
+                  className="upload-icon"
+                />
+                <span className="" style={{ color: "white" }}>
+                  Upload Profile Picture
+                </span>
+              </React.Fragment>
+            )}
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            id="profile-pic"
+            style={{ display: "none" }}
+            onChange={handleFileInputChange}
+          />
+        </div>
         <input
-          type="file"
-          accept="image/*"
-          id="profile-pic"
-          style={{ display: "none" }}
-          onChange={handleFileInputChange}
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Name"
+          className="form-input"
         />
+        <input
+          type="text"
+          name="status"
+          value={formData.status}
+          onChange={handleChange}
+          placeholder="Status"
+          className="form-input"
+        />
+        <button onClick={handleNext} className="next-button">
+          Next
+        </button>
       </div>
-      <input
-        type="text"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-        placeholder="Name"
-        className="form-input"
-      />
-      <input
-        type="text"
-        name="status"
-        value={formData.status}
-        onChange={handleChange}
-        placeholder="Status"
-        className="form-input"
-      />
-      <button onClick={handleNext} className="next-button">
-        Next
-      </button>
     </div>
   );
 }
